81df157657535172ee79558b40385a30827032c6
Brian Dang <heybdang@gmail.com>
2018-01-24 21:47:24 -0500

    output functionality

--- find_poi.py
+++ find_poi.py
@@ -1,6 +1,7 @@
 import sys
 import pandas as pd
 from collections import deque
+import csv
 
 def favorable_min_increment(previous, current, maxCheckpoints, threshold):
     # peek the popping tuple (the best max checkpoint so far)
@@ -105,14 +106,18 @@ def search_window(window, threshold):
     # WIP: currently force directionality
     # i.e. the program only works for uptrending breakouts, not downtrending
     if indexMax < indexMin:
-        return False, 0, 0
+        return False, 0, 0, 0, 0
 
+    # redundant / also search_window() barely does anything...
     valid, indexStart, indexEnd, xStart, xEnd = narrow_start_end(indexMin, indexMax, window, threshold)
-    return valid, xStart, xEnd
+    return valid, indexStart, indexEnd, xStart, xEnd
 
-def main(inputFilePath, threshold, windowSize):
+def main(inputFilePath, threshold, windowSize, outputFilePath):
     # load the csv data into a dataframe
     data = pd.read_csv(inputFilePath, names=["x", "y"])
+
+    # WIP: be wary of breakout pairs with the same end index
+    breakoutPairs = set()
 
     # iterate each row on a sliding (slicing) window
     for i in range(data.shape[0]):
@@ -124,20 +129,31 @@ def main(inputFilePath, threshold, windowSize):
             break
 
         # search for points of interest
-        found, xstart, xend = search_window(window, threshold)
+        found, indexStart, indexEnd, xStart, xEnd = search_window(window, threshold)
         if found:
-            print(window)
-            print(str(xstart)+" "+str(xend))
-            break
+            breakoutPairs.add((indexStart, indexEnd))
 
     # save the poi
+    outputfile = open(outputFilePath, 'w')
+    writer = csv.DictWriter(outputfile, fieldnames=["index_start", "index_end", "x_start", "x_end"], delimiter=',')
+    writer.writeheader()
+    for poi in breakoutPairs:
+        writer.writerow({
+                        "index_start": poi[0],
+                        "index_end": poi[1],
+                        "x_start": data["x"][poi[0]],
+                        "x_end": data["x"][poi[1]]
+                        })
+
+
+
 
 # WIP: could use more robust validation
 def valid_args(args):
-    return len(args) == 4
+    return len(args) == 5
 
 # initial handling
 if __name__ == "__main__":
     if not valid_args(sys.argv):
         sys.exit(-1)
-    main(sys.argv[1], float(sys.argv[2])/100.0, int(sys.argv[3]))
+    main(sys.argv[1], float(sys.argv[2])/100.0, int(sys.argv[3]), sys.argv[4])
